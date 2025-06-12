90fff692f77ebba19054fcee03d1feb1d183d37e
Surbhi Verma <surbhiverma@Surbhis-MacBook-Pro.local>
2025-06-12 11:59:43 -0500

    Add existing code

---
+++ app/details/styles.ts
@@ -0,0 +1,73 @@
+const style = {
+    meetingSummaryCardContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        marginTop: '100px',
+        marginLeft: '250px',
+        marginRight: '20px'
+    },
+    meetingSummaryCard: {
+        display: 'flex',
+        border: '2px solid blue',
+        borderRadius: '10px',
+        overflow: 'hidden',
+        width: '300px',
+    },
+    date: {
+        backgroundColor: '#E0E0FF',
+        padding: '10px',
+        textAlign: 'center',
+    },
+    month: {
+        fontSize: '16px',
+        fontWeight: 'bold',
+    },
+    day: {
+        fontSize: '24px',
+        fontWeight: 'bold',
+    },
+    details: {
+        flexGrow: 1,
+        padding: '10px',
+    },
+    name: {
+        fontSize: '18px',
+        fontWeight: 'bold',
+    },
+    summary: {
+        fontStyle: 'italic',
+    },
+    tags: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        padding: '10px',
+    },
+    tag: {
+        backgroundColor: '#F0F0F0',
+        borderRadius: '5px',
+        padding: '5px',
+        margin: '2px 0',
+        textAlign: 'center',
+    },
+};
+export default style;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
