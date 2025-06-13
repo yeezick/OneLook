1fc5990e20f32ce146c3267d70e852ad2807ff47
Erick Manrique <ramerick5@gmail.com>
2025-06-12 22:39:19 -0500

    support env key

--- app/home/Chat.tsx
+++ app/home/Chat.tsx
@@ -1,33 +1,34 @@
-import LlmConnector, { OpenaiProvider } from '@rcb-plugins/llm-connector';
-import React from 'react';
-import ChatBot from 'react-chatbotify';
+import LlmConnector, { OpenaiProvider } from "@rcb-plugins/llm-connector";
+import React from "react";
+import ChatBot from "react-chatbotify";
 const Chat = () => {
-    let apiKey = "";
-    const plugins = [LlmConnector()];
+  const apiKey = import.meta.env.VITE_OAI_API_KEY;
+  const plugins = [LlmConnector()];
 
-    const flow = {
-		start: {
-			message: "Hello! This is assistant! Please ask question about your meeting.",
-			path: "openai"
-		},
-		openai: {
-			llmConnector: {
-				// provider configuration guide:
-				// https://github.com/React-ChatBotify-Plugins/llm-connector/blob/main/docs/providers/OpenAI.md
-				provider: new OpenaiProvider({
-					mode: 'direct',
-					model: 'gpt-4.1-nano',
-					responseFormat: 'stream',
-					apiKey: apiKey,
-				}),
-				outputType: 'character',
-			},
-		},
-	};
-    return (
-        <div>
-            <ChatBot plugins={plugins} flow={flow}/>
-        </div>
-    );
+  const flow = {
+    start: {
+      message:
+        "Hello! This is assistant! Please ask question about your meeting.",
+      path: "openai",
+    },
+    openai: {
+      llmConnector: {
+        // provider configuration guide:
+        // https://github.com/React-ChatBotify-Plugins/llm-connector/blob/main/docs/providers/OpenAI.md
+        provider: new OpenaiProvider({
+          mode: "direct",
+          model: "gpt-4.1-nano",
+          responseFormat: "stream",
+          apiKey: apiKey,
+        }),
+        outputType: "character",
+      },
+    },
+  };
+  return (
+    <div>
+      <ChatBot plugins={plugins} flow={flow} />
+    </div>
+  );
 };
-export default Chat
+export default Chat;
